3cd92436408b3f13a50b108dc816c9dd71c3b8ec
Anton K. Anderson <Antongrozny@gmail.com>
2024-09-19 17:59:56 -0600

    Removed redundant setBoard() method. Don't need it right now. Phase 0 completed! YAY!!

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -102,8 +102,4 @@ public class ChessBoard {
     public int getBoardSize() {
         return boardSize;
     }
-
-    public void setBoardSize(int boardSize) {
-        this.boardSize = boardSize;
-    }
 }
