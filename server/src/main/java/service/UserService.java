86020bedc01dd33d0bb8826ba5fe7ef68d76df1c
Anton K. Anderson <Antongrozny@gmail.com>
2024-10-19 21:27:03 -0600

    Implemented UserService.java. No idea if it works yet. That's what unit tests are for.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,16 +1,53 @@
 package service;
 
+import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
 import model.AuthData;
 import model.UserData;
+import java.util.UUID;
+
+
 
 public class UserService {
-        public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
-            return null; // TODO: not implemented yet
+        private final UserDAO userDAO;
+
+        private final AuthDAO authDAO;
+
+        public UserService(UserDAO userDAO, AuthDAO authDAO){
+            this.userDAO = userDAO;
+            this.authDAO = authDAO;
+        }
+
+        public static String generateToken() {
+            return UUID.randomUUID().toString();
+        }
+
+        public RegisterResult register(RegisterRequest registerRequest) throws Exception {
+            userDAO.getUser(registerRequest.username());
+            if(userDAO.getUser(registerRequest.username()) == null){
+                userDAO.createUser(
+                        new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email()));
+                String authToken = generateToken();
+                authDAO.createAuth(new AuthData(authToken, registerRequest.username()));
+                return new RegisterResult(registerRequest.username(), authToken);
+            }
+            throw new Exception("Error: already taken");
+            // TODO: potentially in the future, may change the exception type
+            // create authToken here
         }
         public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
-            return null; // TODO: not implemented yet
+            userDAO.getUser(loginRequest.username());
+            String authToken = generateToken();
+            authDAO.createAuth(new AuthData(authToken, loginRequest.username()));
+            return new LoginResult(loginRequest.username(), authToken);
+            // create authToken here
         }
-        public void logout(LogoutRequest logoutRequest) throws DataAccessException {}
-        // TODO: not implemented yet
+        public void logout(LogoutRequest logoutRequest) throws DataAccessException {
+            // check if authToken is valid
+            authDAO.getAuth(logoutRequest.authToken());
+            authDAO.deleteAuth(logoutRequest.authToken());
+        }
+        // delete authToken
 }
