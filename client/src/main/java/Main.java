37adbdd88b63d67aff31c670cbd316d8c20f6682
Anton K. Anderson <Antongrozny@gmail.com>
2024-11-12 12:00:05 -0700

    Brought the testing functionality for Main.java for testing ChessClient back. Turns out I was worried over nothing.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,13 @@
 import chess.*;
+import ui.ChessClient;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        // Note: this is just for testing. Delete Later.
+        String myURL = "myURL"; // placeholder. Fix later.
+        ChessClient client = new ChessClient(myURL); // TODO: update myURL with an actual serverURL later
+        client.run();
     }
 }
