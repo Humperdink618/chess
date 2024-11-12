b7fd08e367aacda6afa2356e1a4848e5b9383045
Anton K. Anderson <Antongrozny@gmail.com>
2024-11-11 22:20:59 -0700

    Trying to revert Main.java back to it's original setting.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,14 +1,8 @@
 import chess.*;
-import ui.ChessClient;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-        // for testing purposes. may or may not delete this later
-      /*  String serverUrl = "I'm a URL"; // DEFINITELY fix this later.
-        ChessClient client = new ChessClient(serverUrl);
-        client.run();
-       */
     }
 }
